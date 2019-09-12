d9d621bf80e49c077ff7ebb6225a9a19e7c96edc
megan schulte <megan.schulte.pdx@gmail.com>
2019-09-11 21:51:28 -0700

    change navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,23 +3,40 @@ import { Link } from "@reach/router";
 
 function Navbar() {
   return (
-    <div>
-      <Link to="/">megan schulte</Link>
-      <ul>
+    <nav className="navbar">
+      <span className="navbar-toggle" id="js-navbar-toggle">
+        <i className="fas fa-bars"></i>
+      </span>
+
+      <Link to="/" className="logo">
+        megan schulte
+      </Link>
+      <ul className="main-nav" id="js-menu">
         <li>
-          <Link to="/projects">projects</Link>
+          <Link to="/projects" className="nav-links">
+            projects
+          </Link>
         </li>
         <li>
-          <a href="https://github.com/meganschultepdx">github</a>
+          <a href="https://github.com/meganschultepdx" className="nav-links">
+            github
+          </a>
         </li>
         <li>
-          <a href="https://www.linkedin.com/in/megan-schulte-pdx/">linkedin</a>
+          <a
+            href="https://www.linkedin.com/in/megan-schulte-pdx/"
+            className="nav-links"
+          >
+            linkedin
+          </a>
         </li>
         <li>
-          <Link to="/contact">contact</Link>
+          <Link to="/contact" className="nav-links">
+            contact
+          </Link>
         </li>
       </ul>
-    </div>
+    </nav>
   );
 }
 
